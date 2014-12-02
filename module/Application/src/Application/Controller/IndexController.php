d075c72dfe4f0a0811dc855d11c9e6b9e71e8afc
aluno <aluno@micro000.elaborata.lab>
2014-12-02 20:15:12 -0200

    teste

---
+++ module/Application/src/Application/Controller/IndexController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    public function loginAction()
+    {
+    	$this->results = array(
+    		'promocao' => 'LEVE UM E PAGUE DOIS!',
+    		'valor' => 'R$ 20,00'
+    		);
+        return new ViewModel($this->results);
+    }
+}
